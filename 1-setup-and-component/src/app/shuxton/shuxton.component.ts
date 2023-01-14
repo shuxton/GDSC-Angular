a0094561de08414facb8083eebc60398c86cd0fb
Kartikeya <kudlu2004@gmail.com>
2023-01-15 00:28:46 +0530

    Generated my component and added better margins to common pages

--- 1-setup-and-component/src/app/shuxton/shuxton.component.ts
+++ 1-setup-and-component/src/app/shuxton/shuxton.component.ts
@@ -1,33 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
-
 interface Participant {
   id: number;
   name: string;
   link: string;
 }
 
-
 @Component({
   selector: 'app-shuxton',
   templateUrl: './shuxton.component.html',
-  styleUrls: ['./shuxton.component.css']
+  styleUrls: ['./shuxton.component.css'],
 })
 export class ShuxtonComponent implements OnInit {
-
   //Add your name and link to the array below
 
-  participants : Participant[]= [{id:1,name: 'Shubham', link: 'shuxton'},
-  {
-    id:2,
-    name: 'Shubham NJ',
-    link: 'shubham-nj'
-  }]
+  participants: Participant[] = [
+    { id: 1, name: 'Shubham', link: 'shuxton' },
+    {
+      id: 2,
+      name: 'Shubham NJ',
+      link: 'shubham-nj',
+    },
+    {
+      id: 3,
+      name: 'Kartikeya Saini',
+      link: 'kartikeya',
+    },
+  ];
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
